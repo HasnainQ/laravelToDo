832c92b3299fb7a6c486477e97c074c61840f863
unknown <qureshihasnain563@gmail.com>
2021-07-31 20:21:57 +0500

    fullstack commit

---
+++ app/Models/Task.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
